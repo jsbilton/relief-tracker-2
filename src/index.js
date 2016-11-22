68573ccc704ecd7e956b72eb66c2f34548dd2bf1
Jared Bilton <jsbilton@gmail.com>
2016-11-22 14:24:30 -0500

    setup react application

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const React = require('react')
-
 const ReactDOM = require('react-dom')
 
-const App = () => <h1>HELLO, React</h1>
+const App = () => <h1>Hello, React</h1>
 
 ReactDOM.render(<App />, document.getElementById('root'))
